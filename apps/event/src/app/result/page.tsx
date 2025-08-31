80cac72a4d09706277642c7fcb2df8e91bcbf46f
eilyri <khaeun1029@gmail.com>
2025-08-31 20:38:49 +0900

    fix: 결과 확인후 삭제 -> 만료시 삭제로 변경

--- apps/event/src/app/result/page.tsx
+++ apps/event/src/app/result/page.tsx
@@ -4,14 +4,14 @@ import WinComponent from "./components/WinComponent";
 import LoseComponent from "./components/LoseComponent";
 
 function ResultContent({ outcome }: { outcome: string | null }) {
-    if (outcome === "win") {
-      return <WinComponent />;
-    } else if (outcome === "lose") {
-      return <LoseComponent />;
-    }
-  
-    return <div>결과를 불러오는 중...</div>;
+  if (outcome === "win") {
+    return <WinComponent />;
+  } else if (outcome === "lose") {
+    return <LoseComponent />;
   }
+
+  return <div>결과를 불러오는 중...</div>;
+}
 
 export default function GameResult() {
   const [outcome, setOutcome] = useState<string | null>(null);
@@ -27,9 +27,8 @@ export default function GameResult() {
       if (elapsed < 5 * 60 * 1000) {
         setOutcome(outcome);
       } else {
-        console.log("결과가 만료됨");
+        sessionStorage.removeItem("gameResult");
       }
-      sessionStorage.removeItem("gameResult");
     }
   }, []);
 
