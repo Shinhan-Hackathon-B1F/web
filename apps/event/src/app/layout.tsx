63241e3a40e53516a5431f5fe3f88ddc1167b9a1
eilyri <khaeun1029@gmail.com>
2025-09-02 15:41:26 +0900

    design: 모바일 시작페이지 디자인 적용

--- apps/event/src/app/layout.tsx
+++ apps/event/src/app/layout.tsx
@@ -1,34 +1,78 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Create Next App",
   description: "B1F",
 };
 
+const kbo = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/KBO-Dia-Gothic_medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/KBO-Dia-Gothic_bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-kbo",
+});
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/Pretendard-Black.otf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/Pretendard-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/Pretendard-SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/Pretendard-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/Pretendard-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pretendard",
+});
+
+const digit = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/DigitalNumbers-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    }
+  ],
+  variable: "--font-digit",
+});
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${kbo.variable} ${pretendard.variable} ${digit.variable}`}>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
---
+++ apps/event/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        kbo: ["var(--font-kbo)"],
+        pretendard: ["var(--font-pretendard)"],
+        digit: ["var(--font-digit)"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
